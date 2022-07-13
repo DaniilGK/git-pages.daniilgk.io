e52dfff3f4c8d88b3ce1ba1fda0ee7ee30e53669
Daniil Gerasimyonok <gerasimenokd638@gmail.com>
2022-07-13 19:53:00 +0300

    add function add button

---
+++ randomFilms/allLets.js
@@ -0,0 +1,6 @@
+export let addButton = document.querySelector(".add-button");
+export let list = document.querySelector(".list");
+export let pig = document.querySelector(".pig");
+export let block = document.querySelector(".block");
+export let numberFilms = document.querySelector(".number-films");
+export let del = document.querySelector(".del");
